30e586bfc97cae016637486962c0defcdfbcc0d4
Sam Selikoff <sam.selikoff@gmail.com>
2025-11-18 05:59:34 -0500

    wip

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   cacheComponents: true,
+
   cacheMaxMemorySize: 0,
   // experimental: {
   //   reactDebugChannel: true,
