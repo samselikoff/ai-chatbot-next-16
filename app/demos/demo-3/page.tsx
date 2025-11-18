47911ff3231feb7de54bb6adf75aefe4b7209bc0
Sam Selikoff <sam.selikoff@gmail.com>
2025-11-17 23:01:06 -0500

    wip

--- app/demos/demo-3/page.tsx
+++ app/demos/demo-3/page.tsx
@@ -4,5 +4,8 @@ import Client from './client';
 export default async function Page() {
   const messages = await db.query.demoMessages.findMany();
 
+  // Artificial delay
+  await new Promise((resolve) => setTimeout(resolve, 500));
+
   return <Client messages={messages} />;
 }
